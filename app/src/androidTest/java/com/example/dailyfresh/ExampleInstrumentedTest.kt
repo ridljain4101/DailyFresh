0e62488bf9b70a402a8113371281b7e00153f9d1
RIDHIM41001 <f20201617@pilani.bits-pilani.ac.in>
2022-03-14 15:08:48 +0530

    ds

---
+++ app/src/androidTest/java/com/example/dailyfresh/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package com.example.dailyfresh
+
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+
+
+    }
+}
